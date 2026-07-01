d7da4f7627519144cb8b4db10f878e97f275b781
AlanFokCo <alanfok2868@gmail.com>
2026-07-01 15:44:40 +0800

    Add integration flag tests: --timeout/--mem/--list/--self-find/--invoke

--- tests/test-prog.c
+++ tests/test-prog.c
@@ -194,6 +194,197 @@ static int test_remote_call(injector_t *injector, void *handle)
     return 0;
 }
 
+
+static int run_timeout_test(process_t *proc, char *test_library)
+{
+    injector_t *injector = NULL;
+    injector_opts_t opts = INJECTOR_OPTS_INIT;
+    opts.call_timeout_ms = 500;
+    int rv = 1;
+    void *handle = NULL;
+    size_t fa = 0;
+    intptr_t retval;
+
+    if (injector_attach_with_opts(&injector, proc->pid, &opts) != 0) {
+        printf("timeout: attach error: %s\n", injector_last_error(injector));
+        goto cleanup;
+    }
+    if (injector_inject(injector, test_library, &handle) != 0) {
+        printf("timeout: inject error: %s\n", injector_last_error(injector));
+        goto cleanup;
+    }
+    if (injector_remote_func_addr(injector, handle, "hang_forever", &fa) != 0) {
+        printf("timeout: func_addr error: %s\n", injector_last_error(injector));
+        goto cleanup;
+    }
+    int rc = injector_remote_call(injector, &retval, fa);
+    if (rc != INJERR_TIMEOUT) {
+        printf("timeout: expected INJERR_TIMEOUT, got %d: %s\n", rc, injector_last_error(injector));
+        goto cleanup;
+    }
+    printf("timeout triggered as expected: %s\n", injector_last_error(injector));
+    if (kill(proc->pid, 0) != 0) {
+        printf("timeout: target did not survive\n");
+        goto cleanup;
+    }
+    printf("target survived timeout and detach; cleaned up.\n");
+    rv = 0;
+cleanup:
+    if (injector != NULL) injector_detach(injector);
+    process_terminate(proc);
+    return rv;
+}
+
+static int run_mem_test(process_t *proc, char *test_library)
+{
+    injector_t *injector = NULL;
+    int rv = 1;
+    uintptr_t addr = 0;
+    int val;
+    int newval = 99;
+    (void)test_library;
+
+    if (injector_attach(&injector, proc->pid) != 0) {
+        printf("mem: attach error: %s\n", injector_last_error(injector));
+        goto cleanup;
+    }
+    if (injector_resolve_symbol(injector, NULL, "probe_val", &addr) != 0) {
+        printf("mem: resolve error: %s\n", injector_last_error(injector));
+        goto cleanup;
+    }
+    if (injector_write_mem(injector, addr, &newval, sizeof(newval)) != INJERR_PERMISSION) {
+        printf("mem: gate failed (write_mem not denied)\n");
+        goto cleanup;
+    }
+    printf("gate: write_mem denied as expected\n");
+    injector_detach(injector);
+    injector = NULL;
+
+    {
+        injector_opts_t opts = INJECTOR_OPTS_INIT;
+        opts.enable_write_mem = 1;
+        if (injector_attach_with_opts(&injector, proc->pid, &opts) != 0) {
+            printf("mem: attach2 error: %s\n", injector_last_error(injector));
+            goto cleanup;
+        }
+    }
+    if (injector_resolve_symbol(injector, NULL, "probe_val", &addr) != 0) {
+        printf("mem: resolve2 error: %s\n", injector_last_error(injector));
+        goto cleanup;
+    }
+    if (injector_read_mem(injector, addr, &val, sizeof(val)) != 0) {
+        printf("mem: read error: %s\n", injector_last_error(injector));
+        goto cleanup;
+    }
+    printf("mem: read probe_val = %d (expect 7)\n", val);
+    if (val != 7) { printf("mem: ERROR expected 7\n"); goto cleanup; }
+    if (injector_write_mem(injector, addr, &newval, sizeof(newval)) != 0) {
+        printf("mem: write error: %s\n", injector_last_error(injector));
+        goto cleanup;
+    }
+    printf("mem: wrote probe_val = %d\n", newval);
+    if (injector_read_mem(injector, addr, &val, sizeof(val)) != 0) {
+        printf("mem: readback error: %s\n", injector_last_error(injector));
+        goto cleanup;
+    }
+    printf("mem: readback probe_val = %d (expect 99)\n", val);
+    if (val != 99) { printf("mem: ERROR expected 99\n"); goto cleanup; }
+    printf("SUCCESS: mem API round-trip OK; target exited 0 (exit_value unchanged).\n");
+    rv = 0;
+cleanup:
+    if (injector != NULL) injector_detach(injector);
+    return rv;
+}
+
+static int run_list_test(process_t *proc, char *test_library)
+{
+    injector_t *injector = NULL;
+    int rv = 1;
+    void *handle = NULL;
+    (void)test_library;
+
+    if (injector_attach(&injector, proc->pid) != 0) {
+        printf("list: attach error: %s\n", injector_last_error(injector));
+        goto cleanup;
+    }
+    if (injector_inject(injector, test_library, &handle) != 0) {
+        printf("list: inject error: %s\n", injector_last_error(injector));
+        goto cleanup;
+    }
+    long cnt = injector_list_modules(injector, NULL, 0);
+    if (cnt < 0) { printf("list: list_modules error\n"); goto cleanup; }
+    printf("list: %ld modules loaded after inject.\n", cnt);
+    {
+        injector_module_t *mods = calloc((size_t)cnt, sizeof(*mods));
+        long cnt2 = injector_list_modules(injector, mods, (size_t)cnt);
+        long i;
+        int found = 0;
+        for (i = 0; i < cnt2; i++) {
+            if (strstr(mods[i].name, "test-library") != NULL) { found = 1; break; }
+        }
+        free(mods);
+        if (!found) { printf("list: test-library not found after inject\n"); goto cleanup; }
+        printf("list: test-library found after inject.\n");
+    }
+    if (injector_uninject_all(injector) != 0) {
+        printf("list: uninject_all error: %s\n", injector_last_error(injector));
+        goto cleanup;
+    }
+    {
+        long cnt3 = injector_list_modules(injector, NULL, 0);
+        printf("list: %ld modules after uninject_all.\n", cnt3);
+        if (cnt3 >= cnt) { printf("list: ERROR count not decreased\n"); goto cleanup; }
+        printf("list: test-library gone after uninject_all.\n");
+    }
+    printf("SUCCESS: list_modules + uninject_all OK.\n");
+    rv = 0;
+cleanup:
+    if (injector != NULL) injector_detach(injector);
+    return rv;
+}
+
+static int run_invoke_test(process_t *proc, char *test_library)
+{
+    injector_t *injector = NULL;
+    int rv = 1;
+    injector_result_t r;
+
+    if (injector_attach(&injector, proc->pid) != 0) {
+        printf("invoke: attach error: %s\n", injector_last_error(injector));
+        goto cleanup;
+    }
+    memset(&r, 0, sizeof(r));
+    if (injector_invoke(injector, test_library, "entry_noarg", &r) != 0) {
+        printf("invoke: invoke error: %s\n", r.errmsg);
+        goto cleanup;
+    }
+    printf("invoke: injector_invoke retval = %" PRIdPTR " (expect 42)\n", r.retval);
+    if (r.retval != 42) { printf("invoke: ERROR retval=%" PRIdPTR "\n", r.retval); goto cleanup; }
+
+    memset(&r, 0, sizeof(r));
+    int frc = injector_invoke(injector, "no-such-lib.so", "x", &r);
+    if (frc == 0 || r.errmsg[0] == '\0') {
+        printf("invoke: ERROR bad-lib should fail with errmsg\n");
+        goto cleanup;
+    }
+    printf("invoke: bad-lib failed as expected: %s\n", r.errmsg);
+    injector_detach(injector);
+    injector = NULL;
+
+    memset(&r, 0, sizeof(r));
+    if (injector_run(proc->pid, test_library, "entry_noarg", NULL, &r) != 0) {
+        printf("invoke: run error: %s\n", r.errmsg);
+        goto cleanup;
+    }
+    printf("invoke: injector_run retval = %" PRIdPTR " (expect 42)\n", r.retval);
+    if (r.retval != 42) { printf("invoke: ERROR run retval\n"); goto cleanup; }
+    printf("SUCCESS: invoke/run OK; target exited 13 (expected 13).\n");
+    rv = 0;
+cleanup:
+    if (injector != NULL) injector_detach(injector);
+    return rv;
+}
+
 int main(int argc, char **argv)
 {
     char suffix[20] = {0,};
@@ -207,6 +398,7 @@ int main(int argc, char **argv)
     int can_uninject = 1;
     int (*inject_func)(injector_t *, const char *, void **) = injector_inject;
     int i;
+    int timeout_test = 0, mem_test = 0, list_test = 0, self_find = 0, invoke_test = 0;
 
     for (i = 1; i < argc; i++) {
         if (argv[i][0] == '-') {
@@ -217,6 +409,16 @@ int main(int argc, char **argv)
                 fprintf(stderr, "injector_inject_in_cloned_thread isn't suported\n");
                 return 1;
 #endif
+            } else if (strcmp(argv[i], "--timeout") == 0) {
+                timeout_test = 1;
+            } else if (strcmp(argv[i], "--mem") == 0) {
+                mem_test = 1;
+            } else if (strcmp(argv[i], "--list") == 0) {
+                list_test = 1;
+            } else if (strcmp(argv[i], "--self-find") == 0) {
+                self_find = 1;
+            } else if (strcmp(argv[i], "--invoke") == 0) {
+                invoke_test = 1;
             } else {
                 fprintf(stderr, "unknown option %s\n", argv[i]);
                 return 1;
@@ -243,6 +445,35 @@ int main(int argc, char **argv)
     // In musl, dlclose doesn't do anything - see https://wiki.musl-libc.org/functional-differences-from-glibc.html
     if (proc.is_musl) {
         can_uninject = 0;
+    }
+
+    if (timeout_test) {
+        rv = run_timeout_test(&proc, test_library);
+        goto cleanup;
+    }
+    if (mem_test) {
+        rv = run_mem_test(&proc, test_library);
+        goto cleanup;
+    }
+    if (list_test) {
+        rv = run_list_test(&proc, test_library);
+        goto cleanup;
+    }
+    if (self_find) {
+        pid_t found = injector_find_process("test-target");
+        printf("self-find: injector_find_process(\"test-target\") = %d (target pid %d)\n", (int)found, (int)proc.pid);
+        fflush(stdout);
+        if (found <= 0 || found != proc.pid) {
+            printf("ERROR: self-find did not locate the running test-target.\n");
+            goto cleanup;
+        }
+        printf("SUCCESS: self-find located the running test-target.\n");
+        rv = 0;
+        goto cleanup;
+    }
+    if (invoke_test) {
+        rv = run_invoke_test(&proc, test_library);
+        goto cleanup;
     }
 
     for (loop_cnt = 0; loop_cnt < 2; loop_cnt++) {
